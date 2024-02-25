0497f8c9257637f1ad5c586f31768b5d6ca0fa49
Meirymm <m_sultan@kbtu.kz>
2024-02-25 20:16:09 +0600

    vx

--- tsis_5/regex.py/1.py
+++ tsis_5/regex.py/1.py
@@ -1,8 +1,8 @@
 import re
+#encoding="utf-8" for read russian text
+with open("row.txt") as file:
+    data = file.read()
 
-with open("row.txt", 'r', encoding="utf-8") as f:
-    data = f.read()
-print(data)
-matches = re.findall(r"аб*", data)
+matches = re.findall(r"аb*", data)
 print(matches)
 
